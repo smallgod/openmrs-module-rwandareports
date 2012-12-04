ea0ae800f6a43aa5b3e3f01a1174ba83f8e1fe9c
gneza <gneza@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-12-04 07:35:53 +0000

    definition to check curent drug order

---
+++ api/src/main/java/org/openmrs/module/rwandareports/definition/CurrentPatientDrugOrder.java
@@ -0,0 +1,33 @@
+package org.openmrs.module.rwandareports.definition;
+
+import org.openmrs.Concept;
+import org.openmrs.module.rowperpatientreports.patientdata.definition.BasePatientData;
+import org.openmrs.module.rowperpatientreports.patientdata.definition.ResultFilter;
+import org.openmrs.module.rowperpatientreports.patientdata.definition.RowPerPatientData;
+
+
+
+public class CurrentPatientDrugOrder extends BasePatientData implements RowPerPatientData {
+
+	private String drugName;
+	private ResultFilter resultFilter;
+
+	public String getDrugName() {
+		return drugName;
+	}
+
+	public void setDrugName(String drugName) {
+		this.drugName = drugName;
+	}
+
+	public ResultFilter getResultFilter() {
+		return resultFilter;
+	}
+
+	public void setResultFilter(ResultFilter resultFilter) {
+		this.resultFilter = resultFilter;
+	}
+	
+	
+	
+}
