0a7315e977338943b83ea417afb4156c24567ac1
gneza <gneza@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-12-04 08:24:04 +0000

    new filter to display the accompagnateur name or none if not present

---
+++ api/src/main/java/org/openmrs/module/rwandareports/filter/AccompagnateurDisplayFilter.java
@@ -0,0 +1,22 @@
+package org.openmrs.module.rwandareports.filter;
+
+import org.openmrs.module.rowperpatientreports.patientdata.definition.ResultFilter;
+
+public class AccompagnateurDisplayFilter implements ResultFilter{
+	private String result;
+	public Object filter(Object value) {
+		String accompagnateurName = (String)value;
+		if (accompagnateurName.length() > 0) 
+		result = accompagnateurName;
+		else
+		result = "None";
+		
+		return result.toString();
+	}
+
+	public Object filterWhenNull() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
