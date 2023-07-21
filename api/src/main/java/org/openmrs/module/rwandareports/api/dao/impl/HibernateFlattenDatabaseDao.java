c9227cefa4863a0e2fead0de546cf4ac3c56fb7a
smallGod <davies.mugume@gmail.com>
2023-07-21 07:20:59 +0200

    reset liquibase changeset id

--- api/src/main/java/org/openmrs/module/rwandareports/api/dao/impl/HibernateFlattenDatabaseDao.java
+++ api/src/main/java/org/openmrs/module/rwandareports/api/dao/impl/HibernateFlattenDatabaseDao.java
@@ -8,22 +8,20 @@ import org.openmrs.module.rwandareports.api.dao.FlattenDatabaseDao;
  * @date: 01/03/2023
  */
 public class HibernateFlattenDatabaseDao implements FlattenDatabaseDao {
-
-    private DbSessionFactory sessionFactory;
-
-    @Override
-    public void executeFlatteningScript() {
-
-        sessionFactory.getCurrentSession()
-                .createSQLQuery("CALL sp_mamba_data_processing_etl()")
-                .executeUpdate();
-    }
-
-    public DbSessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
-    public void setSessionFactory(DbSessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+	
+	private DbSessionFactory sessionFactory;
+	
+	@Override
+	public void executeFlatteningScript() {
+		
+		sessionFactory.getCurrentSession().createSQLQuery("CALL sp_mamba_data_processing_etl()").executeUpdate();
+	}
+	
+	public DbSessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	public void setSessionFactory(DbSessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 }
