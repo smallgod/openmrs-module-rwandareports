9b5c5ad8846e7f4356f324877537f08788919f54
smallgod <davies.mugume@gmail.com>
2024-08-15 17:18:52 +0200

    Removed hardcoded service Id values

--- omod/src/main/resources/_etl/derived/billing/dimensions/dim_billing_report_columns/sp_mamba_dim_billing_report_columns_insert.sql
+++ omod/src/main/resources/_etl/derived/billing/dimensions/dim_billing_report_columns/sp_mamba_dim_billing_report_columns_insert.sql
@@ -1,15 +1,43 @@
 -- $BEGIN
 
-INSERT INTO mamba_dim_billing_report_columns (report_type,
-                                              hop_service_id,
-                                              column_name)
-SELECT "INSURANCE",
-       service_id,
-       name
+-- Fetch and set property values from global_props table
+SET @insurance_property_value = (SELECT property_value
+                                 FROM mamba_source_db.global_property
+                                 WHERE property = 'mohbilling.insuranceReportColumns');
+SET @imaging_property_value = (SELECT property_value
+                               FROM mamba_source_db.global_property
+                               WHERE property = 'mohbilling.IMAGING');
+SET @procedures_property_value = (SELECT property_value
+                                  FROM mamba_source_db.global_property
+                                  WHERE property = 'mohbilling.PROCEDURES');
+
+-- Create a temporary table to store the service_ids and their group names
+CREATE TEMPORARY TABLE temp_service_groups AS
+SELECT service_id,
+       CASE
+           WHEN FIND_IN_SET(service_id, @insurance_property_value) THEN 'INSURANCE'
+           WHEN FIND_IN_SET(service_id, @imaging_property_value) THEN 'IMAGING'
+           WHEN FIND_IN_SET(service_id, @procedures_property_value) THEN 'PROCEDURES'
+           END AS group_name
+FROM mamba_dim_hop_service
+WHERE FIND_IN_SET(service_id, @insurance_property_value)
+   OR FIND_IN_SET(service_id, @imaging_property_value)
+   OR FIND_IN_SET(service_id, @procedures_property_value);
+-- preserves order of insertion (use on small datasets)
+
+
+INSERT INTO mamba_dim_billing_report_columns (report_type, hop_service_id, column_name, group_column_name)
+SELECT 'INSURANCE'    AS report_type,
+       h.service_id   AS hop_service_id,
+       h.name         AS column_name,
+       tsg.group_name AS group_column_name
 FROM mamba_dim_hop_service h
-WHERE h.service_id in (11, 21, 6, 8, 5, 23, 2, 14, 4, 16, 19, 1, 9, 17, 12, 7)
-ORDER BY FIND_IN_SET(h.service_id, '11,21,6,8,5,23,2,14,4,16,19,1,9,17,12,7');
--- heps preserve order of insertion (use on small datasets)
+         JOIN temp_service_groups tsg ON h.service_id = tsg.service_id
+ORDER BY FIND_IN_SET(hop_service_id,
+                     CONCAT_WS(',', @insurance_property_value, @imaging_property_value, @procedures_property_value));
+
+DROP TEMPORARY TABLE IF EXISTS temp_service_groups;
+
 
 -- insert another type here
 
